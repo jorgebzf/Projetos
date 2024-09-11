9f8330274dce56a466e7fe3e255c65b443d63219
Jorge B Fernandes <jorge_bf28@hotmail.com>
2024-09-10 21:27:42 -0300

    Exercício

---
+++ Debug.py
@@ -0,0 +1,17 @@
+#​Código utilizado na aula:
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+#argumentos estão setados de forma divergente int e string, deve ser modificado
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
+#F9 Marcar um Breakpoint
+#F5 Abrir modo Debug
+#F10 Passar por sobre uma linha , Sem entrar em métodos internos
+#F11 Passar por sobre uma linha , e entrar em métodos internos
