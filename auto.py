5a186ddc42d3f908d10b1131fd53c304205e76d8
Jorge B Fernandes <jorge_bf28@hotmail.com>
2024-10-26 15:49:02 -0300

    version

---
+++ auto.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from selenium.webdriver.support.select import Select
+
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,1000', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+# navegar até o site
+driver.get('https://cursoautomacao.netlify.app')
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+checkboxes[0].click()
+checkboxes[2].click()
+input('')
+driver.close()
