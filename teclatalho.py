d2b9f84a8e88c643aa9bb9ed673a40089526c04e
Jorge B Fernandes <jorge_bf28@hotmail.com>
2025-08-13 20:53:40 -0300

    adição de novos arquivos

---
+++ teclatalho.py
@@ -0,0 +1,10 @@
+#Como usar botões de atalho no teclado
+import pyautogui
+#como usar combinações de teclas
+pyautogui.click(1504,44,duration=2)
+#simular segurar uma tecla
+pyautogui.hotkey('ctrl','a')
+pyautogui.hotkey('ctrl','c')
+pyautogui.click(1604,555,duration=2)
+pyautogui.hotkey('ctrl','v')
+
